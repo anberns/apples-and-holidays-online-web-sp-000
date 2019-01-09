fb0fd321d65926e8577a5d3f7b1b4fd82e64f458
Aaron Berns <anberns@gmail.com>
2019-01-09 17:20:43 +0000

    Automatically backed up by Learn

--- working.rb
+++ working.rb
@@ -3,7 +3,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |holiday, supplies|
-      puts "\t#{holiday.to_s.split.capitalize.join} : #{supplies.to_s}"
+      puts "\t#{holiday.to_s.split.capitalize!.join} : #{supplies.to_s}"
       
     end
   end
