1fa741fe625d741038834b5e378c4c761e42cd7e
Aaron Berns <anberns@gmail.com>
2019-01-09 17:08:24 +0000

    Automatically backed up by Learn

--- working.rb
+++ working.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def all_holidays_with_bbq(holiday_hash)
   with_bbq = []
-  holiday_hash[:winter].each do |holiday, supplies|
+  holiday_hash.each do |season, supplies|
     supplies.each do |supply|
       winter_supplies << supply
     end
